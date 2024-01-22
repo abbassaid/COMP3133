42d2a03dbf3d76c8094eaa07ea33b36ef2982a9b
abbassaid <abbas-s-7@hotmail.com>
2024-01-21 20:42:48 -0500

    Uploading Lab One

---
+++ Lab01_101239441_COMP3133/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+// Define the input file path
+const inputFile = 'input_countries.csv';
+
+// Define the output file paths
+const canadaOutputFile = 'canada.txt';
+const usaOutputFile = 'usa.txt';
+
+//Delete canada.txt and usa.txt if they already exist
+fs.unlink(canadaOutputFile, (err) => {
+    if (err) console.log('Error deleting canada.txt:', err);
+});
+fs.unlink(usaOutputFile, (err) => {
+    if (err)  console.log('Error deleting usa.txt:', err);
+});
+//Filter data for Canada and United States and write to text files
+
+const canadaData = [];
+const usaData = [];
+fs.createReadStream(inputFile)
+    .pipe(csv())
+    .on('data', (row) => {
+        const country = row['country'];
+
+        if (country === 'Canada') {
+            canadaData.push(`${row['country']},${row['year']},${row['population']}`);
+        } else if (country === 'United States') {
+            usaData.push(`${row['country']},${row['year']},${row['population']}`);
+        }
+    })
+    .on('end', () => {
+        fs.writeFileSync(canadaOutputFile, canadaData.join('\n'));
+        fs.writeFileSync(usaOutputFile, usaData.join('\n'));
+        console.log('Canada and USA data written to files');
+    })
+    .on('error', (err) => {
+        console.error('Error reading CSV file:', err);
+    });
