e4ac70458bd80a58b74a9a3a248dfc03fc2b4616
abbassaid <abbas-s-7@hotmail.com>
2024-01-27 08:29:56 -0500

    Uploading Lab Two

--- lab2_mocha_test/test/calculator.js
+++ lab2_mocha_test/test/calculator.js
@@ -12,18 +12,18 @@ describe("Calculator", function() {
         assert.equal(Calculator.sub(5, 2), 3)
     })
     it("should return 3 when value is 5 - 2", function() {
-        assert.equal(Calculator.add(5, 2), 5)
+        assert.equal(Calculator.sub(5, 2), 5)
     })
     it("should return 10 when value is 5 * 2", function() {
         assert.equal(Calculator.mul(5, 2), 10)
     })
     it("should return 10 when value is 5 * 2", function() {
-        assert.equal(Calculator.add(5, 2), 12)
+        assert.equal(Calculator.mul(5, 2), 12)
     })
     it("should return 5 when value is 10 / 2", function() {
         assert.equal(Calculator.div(10, 2), 5)
     })
-    it("should return 7 when value is 5 + 2", function() {
+    it("should return 5 when value is 10 / 2", function() {
         assert.equal(Calculator.div(10, 2), 2)
     })
 })
